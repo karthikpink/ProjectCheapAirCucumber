113d9f13fe008ddc2a95e8f9a737d0e8b369fcbb
karthikpink <karthikpink343@gmail.com>
2023-03-17 22:24:34 +0530

    Initial commit

---
+++ src/com/aiite/basicmethod/BasicMethod.java
@@ -0,0 +1,7 @@
+package com.aiite.basicmethod;
+
+public class BasicMethod {
+public static void main(String[] args) {
+	System.out.println("Welcome all to AiiTE");
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author VINOTH
+ *
+ */
+module BasicProject {
+}
