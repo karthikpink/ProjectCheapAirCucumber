719c0dd55c0a6ce97da8bb48edaebbbcfa312a52
jeyaprakash2006 <r.jeyaprakash3@gmail.com>
2023-03-19 20:42:35 +0530

    new commit

--- src/com/aiite/basicmethod/BasicMethod.java
+++
@@ -1,7 +0,0 @@
-package com.aiite.basicmethod;
-
-public class BasicMethod {
-public static void main(String[] args) {
-	System.out.println("Welcome all to AiiTE");
-}
-}
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author VINOTH
- *
- */
-module BasicProject {
-}
